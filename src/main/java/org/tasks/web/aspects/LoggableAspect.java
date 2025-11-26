9f477c40b68f17d75f68180bfa68a703e7c5f576
masha <leyno.masha@gmail.com>
2025-11-27 00:52:42 +0300

    Aspect fix

--- src/main/java/org/tasks/web/aspects/LoggableAspect.java
+++ src/main/java/org/tasks/web/aspects/LoggableAspect.java
@@ -12,19 +12,21 @@ import org.slf4j.LoggerFactory;
 public class LoggableAspect {
     private static final Logger logger = LoggerFactory.getLogger(LoggableAspect.class);
 
-    @Pointcut("within(@org.tasks.web.servlet.annotations.Loggable *) && execution(* * (..))")
-    public void annotateByLoggable() {}
+    @Pointcut("within(@org.tasks.web.annotations.Loggable *) && execution(void * (jakarta.servlet.http.HttpServletRequest, jakarta.servlet.http.HttpServletResponse))")
+    public void annotateHttpMethodsByLoggable() {}
 
-    @Around("annotateByLoggable()")
+    @Around("annotateHttpMethodsByLoggable()")
     public Object logToFile(ProceedingJoinPoint joinPoint) throws Throwable {
         Signature signature = joinPoint.getSignature();
         logger.info("Method {} execution starts.", signature);
+        System.out.printf("Method %s execution starts.%n", signature);
 
         long start = System.currentTimeMillis();
         Object result = joinPoint.proceed();
         long duration = System.currentTimeMillis() - start;
 
         logger.info("Method {} execution finished. Execution time: {} ms.", signature, duration);
+        System.out.printf("Method %s execution finished. Execution time: %d ms.%n", signature, duration);
         return result;
     }
 }
