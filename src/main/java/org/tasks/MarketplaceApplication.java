1d7c56d9bab68b453d4ac7e2227a15ecd6073a64
masha <leyno.masha@gmail.com>
2025-12-07 23:56:40 +0300

    Spring Boot Marketplace Application

--- src/main/java/org/tasks/MarketplaceApplication.java
+++ src/main/java/org/tasks/MarketplaceApplication.java
@@ -2,16 +2,22 @@ package org.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@ComponentScan
 @EnableWebMvc
-public class MarketplaceApplication {
+public class MarketplaceApplication extends SpringBootServletInitializer {
+
     public static void main(String[] args) {
         System.out.println("Starting MarketplaceApplication...");
         SpringApplication.run(MarketplaceApplication.class);
         System.out.println("MarketplaceApplication works!");
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MarketplaceApplication.class);
     }
 }
